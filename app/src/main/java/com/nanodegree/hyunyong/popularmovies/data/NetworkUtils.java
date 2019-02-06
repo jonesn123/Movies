6432800aba9fd588dcc34888b837dbbe33c21cbb
심현용 <jonesn5508@gmail.com>
2019-02-06 16:49:27 +0900

    implement Popular Movie List

---
+++ app/src/main/java/com/nanodegree/hyunyong/popularmovies/data/NetworkUtils.java
@@ -0,0 +1,90 @@
+package com.nanodegree.hyunyong.popularmovies.data;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+    // movie api base url
+    private static final String MOVIE_BASE_URL = "http://api.themoviedb.org/3/movie/";
+
+    // image api base url
+    private static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+    // image size
+    public static final String IMAGE_SIZE_92 = "w92";
+    public static final String IMAGE_SIZE_154 = "w154";
+    public static final String IMAGE_SIZE_185 = "w185";
+    public static final String IMAGE_SIZE_342 = "w342";
+    public static final String IMAGE_SIZE_500 = "w500";
+    public static final String IMAGE_SIZE_780 = "w780";
+    public static final String IMAGE_SIZE_ORIGINAL = "original";
+
+    // my api key in movie DB
+    private static final String API_VALUE = "a0fd9d88f749bbc10d42589347a80e7a";
+
+    // api path for movie restful api
+    final static String POP = "popular";
+    final static String REVIEWS = "reviews";
+
+    // query paraams
+    final static String API_KEY = "api_key";
+
+    // http://api.themoviedb.org/3/movie/popular?api_key=a0fd9d88f749bbc10d42589347a80e7a
+    public static URL buildPopularMovieURL() {
+
+        Uri builtUri = Uri.parse(MOVIE_BASE_URL + POP).buildUpon()
+                .appendQueryParameter(API_KEY, API_VALUE).build();
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    // http://api.themoviedb.org/3/movie/450465/reviews?api_key=a0fd9d88f749bbc10d42589347a80e7a
+    public static URL buildReviewsMovieURL(String movieID) {
+        Uri builtUri = Uri.parse(MOVIE_BASE_URL + movieID + "/" + REVIEWS).buildUpon()
+                .appendQueryParameter(API_KEY, API_VALUE).build();
+        URL url = null;
+
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    public static String getImageUrl(String imageSize185, String movieImageUrl) {
+        return  IMAGE_BASE_URL + imageSize185 + movieImageUrl;
+    }
+}
